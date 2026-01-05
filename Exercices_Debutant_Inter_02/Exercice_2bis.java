794f6461fa659ae0b7153ef9c1654ee4b43a2a2a
Jims42 <jimmycoco.42@outlook.fr>
2026-01-05 08:00:47 +0100

    u

--- Exercices_Debutant_Inter_02/Exercice_2bis.java
+++ Exercices_Debutant_Inter_02/Exercice_2bis.java
@@ -1,56 +1,51 @@
 package Exercices_Debutant_Inter_02;
 
-// Consigne: Crée un tableau de 10 entiers aléatoires entre 1 et 50. 
-
-// Demande à l'utilisateur d'entrer un nombre et indique-lui si ce nombre est présent dans le tableau.
-
 import java.util.Random;
 import java.util.Scanner;
 
-public class Exercice_2 {
+public class Exercice_2bis {
     public static void main(String[] args) {
         Scanner clavier = new Scanner(System.in);
         Random nombres = new Random();
-
+        
         int[] tableau = new int[10];
 
         for (int i = 0; i < tableau.length; i++) {
             tableau[i] = nombres.nextInt(50) + 1;
-            // System.out.print(tableau[i]+" ");
         }
+
         int nombreSaisie = 0;
         int tentative = 0;
         boolean trouve = false;
 
         while (!trouve) {
-
             try {
                 System.out.println("Veuillez entrer un entier entre 1 et 50 : ");
                 nombreSaisie = clavier.nextInt();
                 tentative++;
 
-                for (int alea : tableau) {
-                    if (nombreSaisie == alea) {
+                // Correction ici : on compare avec 'n' qui parcourt le tableau
+                for (int n : tableau) {
+                    if (nombreSaisie == n) {
                         trouve = true;
                         break;
                     }
                 }
-                if (trouve) {
-                    System.out.println("BRAVO ");
 
+                if (trouve) {
+                    System.out.println("BRAVO");
                 } else {
-                    System.out.println("DOMMAGE, essayer encore ");
+                    System.out.println("DOMMAGE, essayez encore");
                 }
+
             } catch (java.util.InputMismatchException e) {
                 System.out.println("ERREUR de saisie");
-                clavier.next();
+                clavier.next(); // Nettoie le scanner
             }
+        } // Cette accolade ferme le WHILE
 
-        }
-
-        System.out.println("Vous avez trouvez le nombre " + nombreSaisie + " en " + tentative + " essais");
-
+        System.out.println("Vous avez trouvé le nombre " + nombreSaisie + " en " + tentative + " essais");
         clavier.close();
-
     }
 }
+
